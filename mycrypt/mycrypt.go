d69d3016d1d0f6f126d42227f0f6fca1a09e6202
Vinh Vu Ai Nguyen <vinhvuainguyen@Vinhs-Air.uia.no>
2023-04-24 10:47:23 +0200

    nye endringer

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,6 +1,6 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -1,49 +1,46 @@
 package mycrypt
 
 import (
-	"reflect"
-	"testing"
+    "reflect"
+    "testing"
 )
-
-// Testene forutsetter et alfabet, som er definert i mycrypt.go som ALF_SEM03
 
 func TestKrypter(t *testing.T) {
 
-	type test struct {
-		inputMessage []rune
-                chiffer int
-		want  []rune
-	}
-	tests := []test{
-		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
-		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
-		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("dnizmocdd7;484c5: 47 6466d45b94c.")},
-                {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
-	}
+    type test struct {
+        inputMessage []rune
+        chiffer      int
+        want         []rune
+    }
+    tests := []test{
+        {inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
+        {inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
+        {inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("bnizmoNcd7;484N5: 47 6466a45S94N.")},
+{inputMessage: []rune("bnizmoNcd7;484N5: 47 6466a45S94N."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
+    }
 
-	for _, tc := range tests {
-		got := Krypter(tc.inputMessage, ALF_SEM03, tc.chiffer)
-		if !reflect.DeepEqual(got, tc.want) {
-			t.Errorf("Feil, for chiffer %d, fikk %q, ønsket %q.", tc.chiffer, got, tc.want)
-		}
-	}
+    for _, tc := range tests {
+        got := Krypter(tc.inputMessage, ALF_SEM03, tc.chiffer)
+        if !reflect.DeepEqual(got, tc.want) {
+            t.Errorf("Feil, for chiffer %d, fikk %q, ønsket %q.", tc.chiffer, got, tc.want)
+        }
+    }
 }
 
-// Posisjonene i alfabetet begynner på 0 fra venstre og teller oppover mot høyre
 func TestSokIAlfabetet(t *testing.T) {
-	type test struct {
-		input rune
-		want  int
-	}
-	tests := []test{
-		{input: 'æ', want: 26},
-		{input: 'a', want: 0},
-	}
+    type test struct {
+        input rune
+        want  int
+    }
+    tests := []test{
+        {input: 'æ', want: 26},
+        {input: 'a', want: 0},
+    }
 
-	for _, tc := range tests {
-		got := sokIAlfabetet(tc.input, ALF_SEM03)
-		if got != tc.want {
-			t.Errorf("Feil, fikk %d, ønsket %d.", got, tc.want)
-		}
-	}
+    for _, tc := range tests {
+        got := sokIAlfabetet(tc.input, ALF_SEM03)
+        if got != tc.want {
+            t.Errorf("Feil, fikk %d, ønsket %d.", got, tc.want)
+        }
+    }
 }
